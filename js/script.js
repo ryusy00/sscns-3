2290ec4dc6002988b3dcff88359164765d0b3076
ryusy00 <152837965+ryusy00@users.noreply.github.com>
2024-06-20 11:08:37 +0900

    initial commit

--- js/script.js
+++ js/script.js
@@ -128,7 +128,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         var triggerPosition = document.getElementById('center-text').offsetTop - window.innerHeight;
         var triggerPosition2 = document.getElementById('business-wrap').offsetTop - window.innerHeight;
         var triggerPosition3 = document.getElementById('solution').offsetTop - window.innerHeight;
-        var triggerPosition4 = document.getElementById('contact-wrap').offsetTop - window.innerHeight;
+        // var triggerPosition4 = document.getElementById('contact-wrap').offsetTop - window.innerHeight;
         var triggerPosition5 = document.getElementById('contact-content').offsetTop - window.innerHeight;
     
         if (scrollPosition > triggerPosition) {
@@ -141,12 +141,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
         if (scrollPosition > triggerPosition3) {
             document.querySelector('#solution > h2').classList.add('animated');
         }
-        if (scrollPosition > triggerPosition4) {
+        // if (scrollPosition > triggerPosition4) {
+        // }
+        if (scrollPosition > triggerPosition5) {
             document.querySelector('#contact-wrap > h2').classList.add('animated');
             document.getElementById('profile').classList.add('animated');
-        }
-        if (scrollPosition > triggerPosition5) {
-            document.getElementById('contact-content').classList.add('animated2');
+            document.getElementById('contact-content').classList.add('animated');
         }
     });
     
